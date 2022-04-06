c8466d610199c45738528fb5ce328c3fef0ecd5f
Lawrence Yee <lawrenceyee521@Lawrences-MacBook-Pro.local>
2022-04-06 15:27:53 -0400

    set up connection

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
+const DATABASE_NAME = "prj5";
 
-const DATABASE_NAME = 'prj5'
-let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true }
-let connectionString =
-process.env.DB_URL || `mongodb://127.0.0.1:27017/${DATABASE_NAME}`;
+let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+let connectionString = process.env.MONGODB_URI;
+
+const MONGODB_URI =
+  connectionString || `mongodb://127.0.0.1:27017/${DATABASE_NAME}`;
 
 // Uncomment to debug Mongoose queries
-mongoose.set('debug', true)
+mongoose.set("debug", true);
 
 // This is for Model.findByIdAndUpdate method, specifically the so that { new: true} is the default
 mongoose.set("returnOriginal", false);
 
 // Setup connection for MongoDB
-mongoose
-  .connect(connectionString, mongooseConfig)
-  .catch((error) =>
-  console.error("Error connecting to MongoDB: ", error.message)
+mongoose.connect(
+  MONGODB_URI,
+  mongooseConfig,
+  () => console.log(chalk.blue(`success connected to the database`)),
+  (err) => console.log(err)
 );
 
 // Listen to MongoDB events
