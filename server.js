6f140f26b27bc5274e4febe53a1375c440099b98
Lawrence Yee <lawrenceyee521@Lawrences-MacBook-Pro.local>
2022-04-26 10:59:20 -0400

    cors edit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import db from "./db/connection.js";
 import routes from "./index.js";
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Establish root
 app.use("/", routes);
+app.use(cors());
 
 db.on("connected", () => {
   console.clear();
